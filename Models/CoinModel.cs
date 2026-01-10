df2214fe297e42fea91b4b86c760dc4cbab10c54
MarionVayt <vusatko@gmail.com>
2026-01-10 13:01:10 +0200

    Add CoinModel.

---
+++ Models/CoinModel.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace CryptoViewer.Models;
+
+public class CoinModel
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("symbol")]
+    public string Symbol { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+    
+    [JsonPropertyName("current_price")]
+    public decimal? CurrentPrice { get; set; }
+    
+    [JsonPropertyName("market_cap_rank")]
+    public int? Rank { get; set; }
+    
+    [JsonPropertyName("price_change")]
+    public double? PriceChange { get; set; }
+}
